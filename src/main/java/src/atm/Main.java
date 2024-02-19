9ba20e83d1bb44d2a94d69bc75aa93175b466d46
Naushad3107 <123984468+Naushad3107@users.noreply.github.com>
2024-02-19 18:14:57 +0530

    Update Main.java

--- src/main/java/src/atm/Main.java
+++ src/main/java/src/atm/Main.java
@@ -4,25 +4,16 @@ import src.atm.gui.GUI;
 
 import static src.atm.util.Constants.INVALID_COMMAND_ARGS_CODE;
 
-/**
- * This class contains the main entry point for the ATM System program.
- * 
- * @author Joshua Ciffer
- * @version 04/09/2018
- */
+
 public final class Main {
 
-	/**
-	 * Don't let anyone instantiate this class.
-	 */
+	
 	private Main() {}
 
 	/**
 	 * Main entry point for the program.
 	 * 
-	 * @param args
-	 *        Command line arguments.
-	 */
+	
 	public static void main(String[] args) {
 		if (args.length == 0) {
 			new GUI();
